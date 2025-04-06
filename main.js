cc8dffa38067a4f46f54b6acf04f69bcbfd84646
Renattoh <renattoh99@gmail.com>
2025-04-06 14:53:20 -0300

    ate a parte 6 min de video

---
+++ main.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form-atividade');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    let linha = '<tr>'
+    linha += `<td>${inputNomeAtividade.value}</td>`
+    linha += `<td>sww`
+});
