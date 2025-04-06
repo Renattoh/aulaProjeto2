da27e758352bb33bffa0d2d66143e1ef10b8343c
Renattoh <renattoh99@gmail.com>
2025-04-06 16:30:52 -0300

    quase o fim

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
+
+
+let linhas = '';
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
@@ -6,6 +12,16 @@ form.addEventListener('submit', function(e) {
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
     let linha = '<tr>'
-    linha += `<td>${inputNomeAtividade.value}</td>`
-    linha += `<td>sww`
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = ''
+    inputNotaAtividade.value = ''
 });
