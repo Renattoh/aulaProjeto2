9744dc5906a3fcfb76c09bc9b641b4986da7c1d5
Renattoh <Renattoh99@gmail.com>
2025-04-17 14:37:30 -0300

    Final

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const atividades = [];
 const notas = [];
 const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
 const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+const notaMinima = parseFloat(prompt('Digite a nota minima:'));
 
 let linhas = '';
 
@@ -20,16 +21,21 @@ function adicionaLinha(){
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
-    atividades.push(inputNomeAtividade.value)
-    notas.push(parseFloat(inputNotaAtividade.value))
+    if (atividades.includes(inputNomeAtividade.value)){
+        alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
+    } else {
+        atividades.push(inputNomeAtividade.value);
+        notas.push(parseFloat(inputNotaAtividade.value));
+    
+        let linha = '<tr>'
+        linha += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
+        linha += '</tr>';
+    
+        linhas += linha;
+    }
 
-    let linha = '<tr>'
-    linha += `<td>${inputNomeAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
-    linha += '</tr>';
-
-    linhas += linha;
 
     inputNomeAtividade.value = ''
     inputNotaAtividade.value = ''
@@ -44,7 +50,7 @@ function atualizaMediaFinal(){
     const mediaFinal = calculaMediaFinal();
 
     document.getElementById('media-final-valor').innerHTML = mediaFinal;
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
     
 }
 
